046ec7ba442514ce23677fe977d3e63ff28d45b8
Kevin Alves <kevin.alves.dev@gmail.com>
2023-04-13 16:52:38 -0300

    add docker support

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(json());
 app.use(carsRouter);
 app.use(handleErrorsMiddleware);
 
-const port = +process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is up and running on port: ${port}`);
-})
+});
